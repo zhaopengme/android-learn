07c248ac7417bb99479136bb12821d34b783e871
zhaopeng <imzhpe@qq.com>
2013-01-14 22:35:10 +0800

    画笔

--- 2013-01-13/customview/src/com/example/customview/CustomView.java
+++ 2013-01-13/customview/src/com/example/customview/CustomView.java
@@ -3,59 +3,70 @@ package com.example.customview;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Bitmap.Config;
+import android.graphics.Paint.Style;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class CustomView extends View {
-	private int mLastX, mLastY;
-	private int mCurrX, mCurrY;
-	private Bitmap mBitmap;
-	private Paint mPaint;
 
-	public CustomView(Context context) {
-		super(context);
+	float preX;
+	float preY;
+	private Path path;
+	public Paint paint = null;
+	final int VIEW_WIDTH = 320;
+	final int VIEW_HEIGHT = 480;
+	Bitmap cacheBitmap = null;
+	Canvas cacheCanvas = null;
+
+	public CustomView(Context context, AttributeSet set) {
+		super(context, set);
+		cacheBitmap = Bitmap.createBitmap(VIEW_WIDTH, VIEW_HEIGHT,
+				Config.ARGB_8888);
+		cacheCanvas = new Canvas();
+		cacheCanvas.setBitmap(cacheBitmap);
+		path = new Path();
+		paint = new Paint(Paint.DITHER_FLAG);
+		paint.setColor(Color.RED);//画笔颜色
+		paint.setStyle(Paint.Style.STROKE);//设置为实心
+		paint.setStrokeWidth(5);//画笔大小
+		paint.setAntiAlias(true);// 防锯齿
+		paint.setDither(true);// 防抖动
 	}
 
-	public CustomView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		mPaint = new Paint();
-		mPaint.setStrokeWidth(5);
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		float x = event.getX();
+		float y = event.getY();
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			path.moveTo(x, y);
+			preX = x;
+			preY = y;
+			break;
+		case MotionEvent.ACTION_MOVE:
+			path.quadTo(preX, preY, x, y);
+			preX = x;
+			preY = y;
+			break;
+		case MotionEvent.ACTION_UP:
+			cacheCanvas.drawPath(path, paint);
+			path.reset();
+			break;
+		}
+		invalidate();
+		return true;
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		int w = getWidth();
-		int h = getHeight();
-		if (mBitmap == null) {
-			mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-		}
-		Canvas tmpCanvas = new Canvas(mBitmap);
-		tmpCanvas.drawLine(mLastX, mLastY, mLastX, mCurrY, mPaint);
-		canvas.drawBitmap(mBitmap, 0, 0, mPaint);
-
+		Paint bmpPaint = new Paint();
+		canvas.drawBitmap(cacheBitmap, 0, 0, bmpPaint);
+		canvas.drawPath(path, paint);
 	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		mLastX = mCurrX;
-		mLastY = mCurrY;
-		mCurrX = (int) event.getX();
-		mCurrY = (int) event.getY();
-
-		switch (event.getAction()) {
-		case MotionEvent.ACTION_DOWN:
-			mLastX = mCurrX;
-			mLastY = mCurrY;
-			break;
-		default:
-			break;
-		}
-
-		invalidate();
-		return true;
-	}
-
 }
