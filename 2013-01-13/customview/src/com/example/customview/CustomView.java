7af48b6e04c9c56e868b9c35ee3abac07aff414d
zhaopeng <imzhpe@qq.com>
2013-01-16 20:44:11 +0800

    自定义视图 ； 一个简单的画图板

--- 2013-01-13/customview/src/com/example/customview/CustomView.java
+++ 2013-01-13/customview/src/com/example/customview/CustomView.java
@@ -26,39 +26,41 @@ public class CustomView extends View {
 	public CustomView(Context context, AttributeSet set) {
 		super(context, set);
 		cacheBitmap = Bitmap.createBitmap(VIEW_WIDTH, VIEW_HEIGHT,
-				Config.ARGB_8888);
-		cacheCanvas = new Canvas();
-		cacheCanvas.setBitmap(cacheBitmap);
-		path = new Path();
-		paint = new Paint(Paint.DITHER_FLAG);
-		paint.setColor(Color.RED);//画笔颜色
-		paint.setStyle(Paint.Style.STROKE);//设置为实心
-		paint.setStrokeWidth(5);//画笔大小
+				Config.ARGB_8888);// 构建一个图像对象，图片质量为ARGB_8888，最高
+		cacheCanvas = new Canvas();// 创建一个画布
+		cacheCanvas.setBitmap(cacheBitmap);// 将图像对象画到画布上面
+		path = new Path();// 创建一个路径对象
+		paint = new Paint();// 创建一个画笔对象
+		paint.setColor(Color.RED);// 画笔颜色
+		paint.setStyle(Paint.Style.STROKE);// 设置为实心
+		paint.setStrokeJoin(Paint.Join.ROUND);
+		paint.setStrokeCap(Paint.Cap.ROUND);
+		paint.setStrokeWidth(25);// 画笔大小
 		paint.setAntiAlias(true);// 防锯齿
 		paint.setDither(true);// 防抖动
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		float x = event.getX();
-		float y = event.getY();
+		float x = event.getX();// 事件发生的x坐标
+		float y = event.getY();// 事件发生的y坐标
 		switch (event.getAction()) {
-		case MotionEvent.ACTION_DOWN:
-			path.moveTo(x, y);
-			preX = x;
-			preY = y;
+		case MotionEvent.ACTION_DOWN:// 按下
+			path.moveTo(x, y);// path移动到x.y
+			preX = x;// 记录此时的x坐标
+			preY = y;// 记录此时的y坐标
 			break;
-		case MotionEvent.ACTION_MOVE:
-			path.quadTo(preX, preY, x, y);
-			preX = x;
-			preY = y;
+		case MotionEvent.ACTION_MOVE:// 移动
+			path.quadTo(preX, preY, x, y);// 画曲线 从最近的preX,preY画曲线到x,y
+			preX = x;// 记录此时的x坐标
+			preY = y;// 记录此时的y坐标
 			break;
-		case MotionEvent.ACTION_UP:
-			cacheCanvas.drawPath(path, paint);
-			path.reset();
+		case MotionEvent.ACTION_UP:// 抬起
+			cacheCanvas.drawPath(path, paint);// 将path路径画到画布上面
+			path.reset();// 重置path
 			break;
 		}
-		invalidate();
+		invalidate();// 绘制刷新view
 		return true;
 	}
 
